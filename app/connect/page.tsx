4ddf850484a6356cbdc40d0bcaded60ed36aaf12
hasanharman <hasanharman33@gmail.com>
2024-07-12 21:12:49 +0300

    chore: Update Connect page metadata and text formatting

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -8,7 +8,7 @@ import SocialMediaButtons from "@/components/social-media-buttons";
 export const metadata: Metadata = {
   title: "Hasan Harman - Connect",
   description:
-    "I'm always open to new opportunities and collaborations. Feel free to reach out to me.",
+    "I'm open to new opportunities and collaborations. Feel free to reach out to me.",
 };
 
 export default function Contact() {
@@ -21,9 +21,9 @@ export default function Contact() {
         height="150"
         className="w-28 h-28 rounded-full border-4"
       />
-      <h2 className="text-2xl font-semibold">Let's Engage</h2>
+      <h2 className="text-2xl font-semibold">Let&apos;s Engage</h2>
       <p className="text-muted-foreground font-light">
-        I'm always open to new opportunities and collaborations.
+        I&apos;m open to new opportunities and collaborations.
         <br />
         Feel free to reach out to me.
       </p>

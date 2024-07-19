6f4615ce6d45a2a1ece4559c667b1b229022bcdb
hasanharman <hasanharman33@gmail.com>
2024-07-19 14:25:12 +0300

    chore: Update sitemap.xml with latest URLs and last modified dates

--- components/table-of-contents/index.tsx
+++ components/table-of-contents/index.tsx
@@ -17,49 +17,53 @@ interface TableOfContentsProps {
   tocList: TocItem[];
 }
 
-const TableOfContents: React.FC<TableOfContentsProps> = ({ tocList }) => {
-  const renderTocItems = (items: TocItem[], isSubItem: boolean = false) => {
-    return items.map((item, index) => (
-      <HoverCard key={index}>
-        <HoverCardTrigger asChild>
-          <div
-            className={`${
-              isSubItem ? "w-5" : "w-7"
-            } flex flex-col items-end h-0.5 bg-slate-400 hover:bg-black rounded-xl my-1`}
-          >
+export default function TableOfContents({ tocList }: TableOfContentsProps) {
+  return (
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <div className="absolute right-1 space-y-2">
+          {tocList.map((item, index) => (
+            <div className="flex flex-col items-end space-y-2" key={item.title}>
+              <div className="w-7 flex flex-col items-end h-0.5 bg-slate-400 hover:bg-black rounded-xl" />
+              {item.items && item.items.length > 0 && (
+                <div className="flex flex-col space-y-2">
+                  {item.items.map((subItem, subIndex) => (
+                    <div
+                      key={subItem.title}
+                      className="w-5 flex flex-col items-end h-0.5 bg-slate-400 hover:bg-black rounded-xl"
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-fit p-3 space-y-2 ">
+        {tocList.map((item, index) => (
+          <div className="space-y-2" key={item.title}>
+            <Link
+              href={item.url}
+              className="text-sm no-underline hover:underline"
+            >
+              {item.title}
+            </Link>
             {item.items && item.items.length > 0 && (
-              <div className="mt-1.5 space-y-2">
-                {renderTocItems(item.items, true)}
+              <div className="flex flex-col space-y-2">
+                {item.items.map((subItem, subIndex) => (
+                  <Link
+                    href={subItem.url}
+                    key={subItem.title}
+                    className="text-xs ml-3 no-underline hover:underline"
+                  >
+                    {subItem.title}
+                  </Link>
+                ))}
               </div>
             )}
           </div>
-        </HoverCardTrigger>
-        <HoverCardContent className="w-fit p-3 space-y-2 ">
-          <Link
-            href={item.url}
-            className="text-sm font-semibold no-underline hover:underline"
-          >
-            {item.title}
-          </Link>
-          {item.items && item.items.length > 0 && (
-            <div className="flex flex-col ml-2 gap-1">
-              {item.items.map((subItem, subIndex) => (
-                <Link
-                  key={subIndex}
-                  href={subItem.url}
-                  className="text-xs no-underline hover:underline"
-                >
-                  {subItem.title}
-                </Link>
-              ))}
-            </div>
-          )}
-        </HoverCardContent>
-      </HoverCard>
-    ));
-  };
-
-  return <div className="absolute right-1 px-1">{renderTocItems(tocList)}</div>;
-};
-
-export default TableOfContents;
+        ))}
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
